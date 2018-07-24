ca70e39e31ad61b51fffdf229f13bec4c148be24
David Wan <davidwan97@gmail.com>
2018-07-23 22:03:13 -0400

    Initial commit

---
+++ Ninja/Source/Ninja/NinjaGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NinjaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NINJA_API ANinjaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
