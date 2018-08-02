d41981ba84a2b3d786336bbbcdad23fd9c88e773
David Wan <davidwan97@gmail.com>
2018-08-01 22:16:09 -0400

    Jumping in the air with no keys pressed does a vertical jump

--- Ninja/Source/Ninja/NinjaMovementComponent.cpp
+++ Ninja/Source/Ninja/NinjaMovementComponent.cpp
@@ -6,13 +6,18 @@
 bool UNinjaMovementComponent::DoJump(bool bReplayingMoves) {
 	if (IsFalling()) {
 		float Right = GetPawnOwner()->GetInputAxisValue("MoveRight");
-		GEngine->AddOnScreenDebugMessage(-1, 1.5, FColor::Blue, FString::Printf(TEXT("Right: %f"), Right));
+		//GEngine->AddOnScreenDebugMessage(-1, 1.5, FColor::Blue, FString::Printf(TEXT("Right: %f"), Right));
 		if (Right != 0.f) {
 			
 			Velocity = GetPawnOwner()->GetActorRightVector() * 600.f * Right;
 			Super::DoJump(bReplayingMoves);
 			return true;
-		} 
+		}
+		else {
+			Velocity = FVector::ZeroVector;
+			Super::DoJump(bReplayingMoves);
+			return true;
+		}
 	}
 	Super::DoJump(bReplayingMoves);
 	return true;
