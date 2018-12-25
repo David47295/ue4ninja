508eb095fd1b13c9e8bb6fd8a35c2d7f1c6f6cae
David Wan <davidwan97@gmail.com>
2018-12-24 21:56:44 -0500

    Added Ninja Game Instance class

---
+++ Ninja/Source/Ninja/NinjaGameInstance.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "NinjaGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NINJA_API UNinjaGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
